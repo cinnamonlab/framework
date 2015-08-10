6fdcc8af5dd7b51d9ee9068a1d139787489c4364
hajime3333out <hotta@fastbid.asia>
2015-08-11 00:33:48 +0800

    remove the risk of not defining __APP__

--- src/Framework/Config.php
+++ src/Framework/Config.php
@@ -1,22 +1,54 @@
 <?php
 namespace Framework;
+use Framework\Exception\FrameworkException;
 
 /**
  * Class Config
  *
  * @package Framework
  */
+
 class Config
 {
 
+    /**
+     * Set
+     *
+     * Basically for testing purpose
+     * @param $key
+     * @param $value
+     */
+
+    public static function set ( $key, $value ) {
+        self::$data[$key] = $value;
+    }
+
+    /**
+     * Get Configuration
+     *
+     * @param $key
+     * @param null $default
+     * @return null
+     */
     public static function get( $key, $default = null ) {
+
+        if ( defined('__APP__') ) $base_path = __APP__;
+        else {
+            $base_path_array = preg_split("/\//", __DIR__);
+            $base_path = "";
+            foreach($base_path_array as $b) {
+                if ( $b == "" ) continue;
+                if ( $b == 'vendor' ) break;
+                $base_path .= "/" . $b;
+            }
+        }
 
         $key_array = preg_split('/\./', $key );
         if ( !isset($key_array[0]) ) return null;
 
         if ( !isset(self::$data[$key_array[0]])) {
             self::$data[$key_array[0]] =
-                include __APP__ . '/config/' . $key_array[0] . '.php';
+                include $base_path . '/config/' . $key_array[0] . '.php';
             if ( self::$data[$key_array[0]] == false ) {
                 self::$data[$key_array[0]] = null;
             }
@@ -25,7 +57,12 @@ class Config
         $return = self::$data;
 
         foreach($key_array as $k) {
-            if ( !isset($return[$k]) ) return $default;
+            if ( !isset($return[$k]) ) {
+                if ( $default instanceof FrameworkException ) {
+                    throw $default;
+                }
+                else return $default;
+            }
             $return = $return[$k];
         }
         return $return;
